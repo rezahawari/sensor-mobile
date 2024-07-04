3dd715494c19ea783b4856268a6c4fab8cae7a40
sulenq <whyimustwriteit@gmail.com>
2024-07-04 16:55:10 +0700

    styling

--- src/pages/main/maps/Maps.tsx
+++ src/pages/main/maps/Maps.tsx
@@ -2,7 +2,6 @@ import { Box } from "@chakra-ui/react";
 import Header from "../../../components/dependent/Header";
 import CContainer from "../../../components/independent/wrapper/CContainer";
 import { useContentBgColor } from "../../../constant/colors";
-import SensorMaps from "../../../components/dependent/SensorMaps";
 
 export default function Maps() {
   // SX
